508494eed8ce54ff3094cbc3e9bf1bacbfd38938
jonathansmirnoff <jonathan@rsk.co>
2020-03-05 16:53:37 -0300

    updated truffle box next box

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,39 +4,44 @@ import Form from 'react-bootstrap/Form';
 import Spinner from 'react-bootstrap/Spinner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react';
-import { web3Mainnet, web3Testnet } from '../utils/web3-util';
+import { web3Mainnet, web3Testnet, getSimpleStorageValue} from '../utils/web3-util';
 
 function Index(props) {
   const UpdateButton = () => (
-    <Button variant="success" onClick={() => updateBestBlocks()}>
-      Update block number
+    <Button variant="success" onClick={() => updateNetworkData()}>
+      Update
     </Button>
   );
 
   const [mainnetBlock, setMainnetBlock] = useState(1);
   const [testnetBlock, setTestnetBlock] = useState(1);
+  const [simpleStorageValue, setSimpleStorageValue] = useState(1);
+  
   const [updateComponent, setUpdateComponent] = useState(<UpdateButton />);
   const [bestBlockLoading, setBestBlockLoading] = useState(true);
 
   useEffect(() => {
-    const initBestBlock = async () => {
+    const initNetworkData = async () => {
       const mainnetBestBlock = await web3Mainnet.eth.getBlockNumber();
       const testnetBestBlock = await web3Testnet.eth.getBlockNumber();
+      const networkSimpleStorageValue = await getSimpleStorageValue();
+      setSimpleStorageValue(networkSimpleStorageValue.toNumber());      
       setMainnetBlock(mainnetBestBlock);
       setTestnetBlock(testnetBestBlock);
       setBestBlockLoading(false);
     }
-    initBestBlock()
+    initNetworkData()
   }, []);
 
-  async function updateBestBlocks() {
+  async function updateNetworkData() {
     setUpdateComponent(<Spinner animation="border" variant="success" />);
     setMainnetBlock(await web3Mainnet.eth.getBlockNumber());
     setTestnetBlock(await web3Testnet.eth.getBlockNumber());
+    setSimpleStorageValue((await getSimpleStorageValue()).toNumber());
     setUpdateComponent(<UpdateButton />);
   }
 
-  const BestBlock = props => {
+  const NetworkData = props => {
     return (
       <>
         {!props.loading ? (
@@ -44,6 +49,8 @@ function Index(props) {
             <h4>Mainnet best block {props.mainnetBlock}</h4>
             <br />
             <h4>Testnet best block {props.testnetBlock}</h4>
+            <br />
+            <h4>Simple Storage Value {props.simpleStorageValue}</h4>
           </div>
         ) : (
           <Spinner animation="border" variant="success" />
@@ -62,9 +69,10 @@ function Index(props) {
       <br />
       <Form>
         <Form.Group>
-          <BestBlock
+          <NetworkData
             mainnetBlock={mainnetBlock}
             testnetBlock={testnetBlock}
+            simpleStorageValue={simpleStorageValue}
             loading={bestBlockLoading}
           />
         </Form.Group>
--- app/utils/web3-util.js
+++ app/utils/web3-util.js
@@ -1,4 +1,5 @@
-var Web3 = require('web3');
+const Web3 = require('web3');
+const SimpleStorageContract = require('../contracts/SimpleStorage.json');
 
 export let web3Mainnet = new Web3(
   new Web3.providers.HttpProvider('https://public-node.rsk.co'),
@@ -6,3 +7,18 @@ export let web3Mainnet = new Web3(
 export let web3Testnet = new Web3(
   new Web3.providers.HttpProvider('https://public-node.testnet.rsk.co'),
 );
+export async function getSimpleStorageValue() {
+  // To connect to RSK network uncomment the following line.
+  // const provider = new Web3.providers.HttpProvider("https://public-node.testnet.rsk.co");
+  // const provider = new Web3.providers.HttpProvider("https://public-node.rsk.co");
+
+  const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545")
+  const web3 = new Web3(provider);
+  const networkId = await web3.eth.net.getId();
+  const deployedNetwork = SimpleStorageContract.networks[networkId];
+  const contractInstance = new web3.eth.Contract(
+    SimpleStorageContract.abi,
+    deployedNetwork && deployedNetwork.address);
+
+  return await contractInstance.methods.get().call();
+} 
